ef11dbc8f2efcddb6d02dd377102f05258d53506
Emil Bruhn <s246070@dtu.dk>
2026-02-18 13:29:37 +0100

    added cookiecutter structure

---
+++ src/dna_modelling/train.py
@@ -0,0 +1,10 @@
+from dna_modelling.model import Model
+from dna_modelling.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
