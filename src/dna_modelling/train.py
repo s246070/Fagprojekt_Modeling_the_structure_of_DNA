40077b55c8d28e312898dfcd4f91f908e89bc729
Emil <s246070@github.com>
2026-03-04 17:58:05 +0100

    added relevant dependencies and deleted unused ones, added submission script

--- src/dna_modelling/train.py
+++ src/dna_modelling/train.py
@@ -2,7 +2,21 @@ from dna_modelling.model import Model
 from dna_modelling.data import MyDataset
 
 def TrainModel(model,train_dataloader, epochs=1,val_dataloader=None, device=None):
-
+    """Trains the model for a given number of epochs.
+    
+    Args:
+        model: The model to be trained.
+        train_dataloader: The dataloader for the training data.
+        epochs: The number of epochs to train for.
+        val_dataloader: The dataloader for the validation data (optional).
+        device: The device to train on (optional).
+    
+    Returns:
+        A tuple containing the training accuracy, validation accuracy, training loss, and validation loss.
+    """
+    
+    
+    torch.set_number_of_threads(1) # brug denne til at sætte antal tråde.
     model.train()
 
     if device is None:
@@ -16,7 +30,7 @@ def TrainModel(model,train_dataloader, epochs=1,val_dataloader=None, device=None
     val_loss = []
 
     for epoch in range(epochs):
-        
+
 
 
 
