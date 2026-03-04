fe34c35630df4577e18a1c370f8a9a1f3f02ed87
Emil <s246070@github.com>
2026-03-04 13:39:09 +0100

    started on training loop

--- src/dna_modelling/train.py
+++ src/dna_modelling/train.py
@@ -1,10 +1,27 @@
 from dna_modelling.model import Model
 from dna_modelling.data import MyDataset
 
-def train():
-    dataset = MyDataset("data/raw")
-    model = Model()
-    # add rest of your training code here
+def TrainModel(model,train_dataloader, epochs=1,val_dataloader=None, device=None):
+
+    model.train()
+
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    model.to(device)
+
+    train_acc = []
+    test_acc = []
+    train_loss = []
+    val_loss = []
+
+    for epoch in range(epochs):
+        
+
+
+
+
+    
 
 if __name__ == "__main__":
     train()
