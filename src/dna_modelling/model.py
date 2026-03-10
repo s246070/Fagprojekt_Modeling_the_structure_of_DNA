077b4cde58ee515c4398b1ee1f2fa65b9f0df4d7
Theis <theishove@gmail.com>
2026-03-10 10:41:13 +0100

    jeg aner ikke hvordan denne model fungere endnu

--- src/dna_modelling/model.py
+++ src/dna_modelling/model.py
@@ -1,16 +1,34 @@
 from torch import nn
 import torch
 
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+class LDM(nn.Module):
+    """"
+    Latent Distance Model (LDM) for DNA modelling. This model learns latent representations for cells and features, 
+    and uses them to reconstruct the original data matrix. 
+    The model is trained using mean squared error loss between the original data and the reconstructed data.
+    """
+    def __init__(self, data, ls_dim, device, epochs, lr, seed=None):
+        super(LDM, self).__init__()
+        self.data = data
+        self.ls_dim = ls_dim
+        self.device = device
 
-if __name__ == "__main__":
-    model = Model()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+        # data dimensions
+        self.cells, self.features = data.shape
+
+        # Hyperparameters
+        self.epochs = epochs
+        self.lr = lr
+
+        # Set random seed for reproducibility
+        self.seed = seed
+        self.__set_seed(seed)
+
+        # Learned parameters
+        self.gamma = nn.Parameter(torch.randn(1, device=device))
+        self.beta = nn.Parameter(torch.randn(1, device=device))
+        self.embed_cells = torch.nn.Parameter(torch.randn(self.cells, self.ls_dim, device=device))
+        self.embed_features = torch.nn.Parameter(torch.randn(self.features, self.ls_dim, device=device))
+
+        
