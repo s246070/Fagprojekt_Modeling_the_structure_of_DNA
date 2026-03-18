422ed7263c92534837de26ce896c3357f7c9a9f8
Theis <theishove@gmail.com>
2026-03-18 16:37:33 +0100

    fixed merges

--- src/dna_modelling/data.py
+++ src/dna_modelling/data.py
@@ -4,13 +4,25 @@ import scanpy as sc
 import scipy.sparse as sp
 import torch
 
+"""
+This class is responsible for downloading and loading the data. 
+It ensures that the data is downloaded to a specific directory 
+and provides a method to load the data into either memory or hard drive.
+"""
 class Data():
-
+    """ 
+    The constructor initializes the data directory, the path to the data file, 
+    and the backup URL for downloading the data if it is not already present. 
+    """
     def __init__(self):
         self.data_directory = Path("data")
         self.data_path = self.data_directory / "lung_atlas_preprocessed.h5ad"
         self.backup_url = "https://figshare.com/ndownloader/files/52859288"
 
+    """
+    This method checks if the data file exists at the specified path. 
+    If it does not exist, it creates the necessary directories and downloads the data from the backup URL.
+    """
     def ensure_data_downloaded(self):
         self.data_directory.mkdir(parents=True, exist_ok=True)
 
@@ -18,7 +30,12 @@ class Data():
             print(f"Downloading datset to {self.data_path}...")
             urlretrieve(self.backup_url, self.data_path)
 
-    def load_data(self, backed=False):
+    """
+    This method loads the data from the specified path. 
+    If the 'backed' parameter is set to True, it loads the data into the hard drive.
+    If 'backed' is False, it loads the entire dataset into memory.
+    """
+    def load_data(self, backed=True):
         self.ensure_data_downloaded()
 
         if backed:
@@ -49,26 +66,7 @@ class Data():
     
 
 
-
-    def anndata_to_tensor(self, adata, device, make_binary=True):
-        X = adata.X
-
-        # Load into memory if backed/lazy
-        if hasattr(X, "__getitem__") and not isinstance(X, (list, tuple)):
-            try:
-                X = X[:]
-            except Exception:
-                pass
-
-        # Convert sparse to dense
-        if sp.issparse(X):
-            X = X.toarray()
-
-        # Convert to tensor
-        X = torch.as_tensor(X, dtype=torch.float32, device=device)
-
-        if make_binary:
-            X = (X > 0).float()
-
-        return X
-        
+if __name__ == "__main__":
+    data = Data()
+    adata = data.load_data(backed=True)
+    print(adata)
