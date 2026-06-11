723d3d38018419db60abe96d8b0ee8c06d39d7ab
TajsTheGreat <theishove@gmail.com>
2026-06-11 11:31:34 +0200

    plot scripts and KNN accuracy script

---
+++ src/benchmarking/KNN.py
@@ -0,0 +1,16 @@
+import torch
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+with open("src/benchmarking/cell_types.txt", "r") as f:
+    cell_types = [line.strip() for line in f]
+
+model = torch.load("models/ldm_ls2_weighting_False_run100.pth")
+
+data = model['embed_cells'].cpu().detach().numpy()
+
+neigh = KNeighborsClassifier(n_neighbors=5, metric='euclidean')
+neigh.fit(data, cell_types)
+pred = neigh.predict(data)
+
+print(accuracy_score(pred, cell_types))
