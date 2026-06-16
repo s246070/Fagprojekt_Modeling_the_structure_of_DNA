83fa3c325bff0d6bd208e875b3ee09869ed39c59
TajsTheGreat <theishove@gmail.com>
2026-06-16 12:48:29 +0200

    Fixes to simba metric so it works like the other metrics

--- src/benchmarking/Metrics_simba.py
+++ src/benchmarking/Metrics_simba.py
@@ -2,19 +2,20 @@ import scanpy as sc
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, normalized_mutual_info_score, adjusted_rand_score
 
-with open("src/benchmarking/cell_types.txt", "r") as f:
+with open("src/benchmarking/cell_types_subset_1.txt", "r") as f:
     cell_types = [line.strip() for line in f]
 
-adata = sc.read_h5ad("results/simba/simba_subset_dim2/adata_cells_simba.h5ad")
+adata = sc.read_h5ad("models/adata_cells_ldm32_simba.h5ad")
 
 data = adata.X
 
-neigh = KNeighborsClassifier(n_neighbors=15, metric="euclidean")
-neigh.fit(data, cell_types)
-pred = neigh.predict(data)
+for i in [15, 30, 50]:
+    neigh = KNeighborsClassifier(n_neighbors=i + 1, metric="euclidean")
+    neigh.fit(data, cell_types)
+    pred = neigh.predict(data)
 
-print(
-    f"Accuracy: {accuracy_score(cell_types, pred)}, "
-    f"NMI: {normalized_mutual_info_score(cell_types, pred)}, "
-    f"ARI: {adjusted_rand_score(cell_types, pred)}"
-)
+    print(
+        f"Accuracy: {accuracy_score(cell_types, pred)}, "
+        f"NMI: {normalized_mutual_info_score(cell_types, pred)}, "
+        f"ARI: {adjusted_rand_score(cell_types, pred)}"
+    )
