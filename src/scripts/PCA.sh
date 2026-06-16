b0437b3d51a83c38bbf1bd85d0a100fce6a65f70
TajsTheGreat <theishove@gmail.com>
2026-06-16 16:31:58 +0200

    KNN on PCA, and KMeans for all models together with NMI and ARI

---
+++ src/scripts/PCA.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#BSUB -J PCA_Script_2
+#BSUB -o outfiles/PCA_Script_2%J.out
+#BSUB -e outfiles/PCA_Script_2%J.err
+#BSUB -q hpc
+#BSUB -n 4
+#BSUB -R "rusage[mem=100GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -W 48:00
+# end of BSUB options
+
+cd "$LS_SUBCWD" || exit 1
+
+module purge
+module load python3/3.12.11
+
+source .venv-cpu/bin/activate
+
+export OMP_NUM_THREADS=$LSB_DJOB_NUMPROC
+export OPENBLAS_NUM_THREADS=$LSB_DJOB_NUMPROC
+export MKL_NUM_THREADS=$LSB_DJOB_NUMPROC
+export NUMEXPR_NUM_THREADS=$LSB_DJOB_NUMPROC
+export MKL_DYNAMIC=false
+export OMP_DYNAMIC=false
+
+
+python src/PCA/main_PCA.py
